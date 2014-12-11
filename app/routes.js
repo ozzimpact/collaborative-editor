5fc7bec6fc20cbf2ad5ca2ec56a58f7c5d16e02e
goldenilkay92 <goldenilkay92@gmail.com>
2014-12-11 19:28:34 +0200

    add login and signup

---
+++ app/routes.js
@@ -0,0 +1,58 @@
+module.exports = function (app, passport) {
+
+
+    app.get('/', function (req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        });
+    });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/login', function (req, res) {
+        res.render('login.ejs', {message: req.flash('loginMessage')});
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
+
+    app.get('/signup', function (req, res) {
+        res.render('signup.ejs', {message: req.flash('signupMessage')});
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
+
+
+
+
+    app.get('/connect/local', function (req, res) {
+        res.render('connect-local.ejs', {message: req.flash('loginMessage')});
+    });
+    app.post('/connect/local', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/connect/local',
+        failureFlash: true
+    }));
+
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+
+        res.redirect('/');
+    }
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+//node server
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
+
+app.set('view engine', 'ejs');
+
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(session({secret: 'secret angular'}));
+
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
